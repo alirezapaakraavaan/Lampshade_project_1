b61b32572d4a773aa7c1c9be264229ac6a0a84e2
Alireza Pakravan <66172389+alirezapaakraavaan@users.noreply.github.com>
2023-10-20 13:28:53 +0330

    ProductCategory Management Implemented

---
+++ Lampshade/ShopManagement.Infrastructure.EFCore/ProductCategoryRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using _0_Framework.Infrastructure;
+using ShopManagement.Application.Contracts.ProductCategory;
+using ShopManagement.Domain.ProductCategoryAgg;
+
+namespace ShopManagement.Infrastructure.EFCore
+{
+    public class ProductCategoryRepository : RepositoryBase<long, ProductCategory>, IProductCategoryRepository
+    {
+        private readonly ShopContext _context;
+
+        public ProductCategoryRepository(ShopContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public EditProductCategory GetDetails(long id)
+        {
+            return _context.ProductCategories.Select(x => new EditProductCategory
+            {
+                Id = x.Id,
+                Description = x.Description,
+                Keywords = x.Keywords,
+                MetaDescription = x.MetaDescription,
+                Name = x.Name,
+                Slug = x.Slug,
+                PictureTitle = x.PictureTitle,
+                Picture = x.Picture,
+                PictureAlt = x.PictureAlt
+            }).FirstOrDefault(x => x.Id == id);
+        }
+
+        public List<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel)
+        {
+            var query = _context.ProductCategories.Select(x => new ProductCategoryViewModel
+            {
+                Id = x.Id,
+                Picture = x.Picture,
+                CreationDate = x.CreationDate.ToString(CultureInfo.InvariantCulture),
+                Name = x.Name
+            });
+
+            if (!string.IsNullOrWhiteSpace(searchModel.Name))
+                query = query.Where(x => x.Name.Contains(searchModel.Name));
+
+            return query.OrderByDescending(x => x.Id).ToList();
+        }
+    }
+}
